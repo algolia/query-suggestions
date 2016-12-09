96e46477c5de041bfba36709556a882fa0d3e335
Matthieu Dumont <jerska@live.fr>
2016-12-09 18:35:31 +0100

    fix(config): type cast numbers

--- connector/src/config.rb
+++ connector/src/config.rb
@@ -5,5 +5,5 @@ CONFIG = {
   'api_key' => ENV['API_KEY'],
   'index_prefix' => ENV['INDEX_PREFIX']
 }.merge(JSON.parse(ENV['CONFIG']))
-CONFIG['min_hits'] ||= 5
-CONFIG['min_letters'] ||= 3
+CONFIG['min_hits'] = (CONFIG['min_hits'] || 5).to_i
+CONFIG['min_letters'] = (CONFIG['min_letters'] || 3).to_i
