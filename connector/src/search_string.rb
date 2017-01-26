43b185f080e0e7ada58bd3476f168be1a476d417
Matthieu Dumont <jerska@live.fr>
2017-01-26 13:21:48 +0100

    fix(SearchString): fix multiline regexp

--- connector/src/search_string.rb
+++ connector/src/search_string.rb
@@ -1,6 +1,6 @@
 module SearchString
   SKIP_REGEXP = /[^\p{L}\s]/
-  KEEP_REGEXP = /^[\p{L}\s]+$/
+  KEEP_REGEXP = /\A[\p{L}\s]+\Z/
 
   def self.clean str
     str.strip.split(/\s+/).join(' ').downcase
